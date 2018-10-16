52c2f6878e6d410e0b89c5c459e8f0469ff44943
Goran Peric <goran.peric@ringieraxelspringer.rs>
2018-10-16 23:13:01 +0200

    added more code

--- epicautofight.js
+++ epicautofight.js
@@ -14,10 +14,34 @@
                 var battles = request.response.battles;
                 for (var battle in battles) {
                     var battle_data = battles[battle];
+                    if (battle_data.type !== "tanks") {
+                        continue;
+                    }
+                    var epic_division = getEpicDivision(battle_data); 
+                    if (epic_division === 0) {
+                        continue;
+                    }
                     console.log(battle_data.id);
+                    console.log(epic_division);
                 }
             }
         }
+    }
+
+    /**
+    *   Returns 0 if there is no epic battle, otherwise, method returns epic division.
+    */
+    function getEpicDivision(battle_data) {
+        var epic_division = 0;
+        var is_epic_battle = false;
+        for (var division in battle.div) {
+            var division_data = battle.div.division;
+            if (division.intensity_scale === "epic_battle") {
+                epic_division = division_data.div;
+                break;
+            }
+        }
+        return epic_division;
     }
 
 })(window.EAF = window.EAF || {});
